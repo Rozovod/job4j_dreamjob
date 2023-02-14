28631ac79183a71c3b4cfaed074395fab675402b
Rozovod <rozovod.store@mail.ru>
2023-02-14 10:33:52 +0300

    2. Связь слоев через Spring DI. [#504856]

--- src/main/java/ru/job4j/dreamjob/repository/MemoryCandidateRepository.java
+++ src/main/java/ru/job4j/dreamjob/repository/MemoryCandidateRepository.java
@@ -10,7 +10,6 @@ import java.util.Optional;
 
 @Repository
 public class MemoryCandidateRepository implements CandidateRepository {
-    private static final MemoryCandidateRepository INSTANCE = new MemoryCandidateRepository();
     private int nextId = 1;
     private final Map<Integer, Candidate> candidates = new HashMap<>();
 
@@ -19,10 +18,6 @@ public class MemoryCandidateRepository implements CandidateRepository {
         save(new Candidate(0, "Stepan Stepanov", ""));
         save(new Candidate(0, "Fedor Fedorov", ""));
         save(new Candidate(0, "Egor Egorov", ""));
-    }
-
-    public static MemoryCandidateRepository getInstance() {
-        return INSTANCE;
     }
 
     @Override
